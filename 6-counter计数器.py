ffa618f21367e18ff0d46dd1e52a11fc6595293b
杨凤玲 <yangfengling@inttech.cn>
2020-08-16 20:50:47 +0800

    Counter()计数器

---
+++ 6-counter计数器.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+from collections import Counter
+
+c = Counter('hell natural laguage')
+print(c)
+
+
+'''
+输出结果如下所示：
+Counter({'l': 4, 'a': 4, 'e': 2, ' ': 2, 'u': 2, 'g': 2, 'h': 1, 'n': 1, 't': 1, 'r': 1})
+'''
