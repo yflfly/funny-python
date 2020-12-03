bc7b9bb167431b40a1927abbfd5f70aeba73bd21
杨凤玲 <yangfengling@inttech.cn>
2020-12-03 22:06:56 +0800

    python100例

---
+++ Python 100例/34.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：练习函数调用。
+
+程序分析：使用函数，输出三次 RUNOOB 字符串。
+'''
+def hello_runoob():
+    print('RUNOOB')
+
+
+def hello_runoobs():
+    for i in range(3):
+        hello_runoob()
+
+
+if __name__ == '__main__':
+    hello_runoobs()
