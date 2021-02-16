a5ff0c013c7df390cec2a347b2b0f1ef81863651
杨凤玲 <yangfengling@inttech.cn>
2021-02-16 19:46:14 +0800

    python100例

---
+++ Python 100例/51.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+学习使用按位与 & 。
+'''
+if __name__ == '__main__':
+    a = 0x77
+    b = a & 3
+    print('a & b = %d' % b)
+    b &= 7
+    print('a & b = %d' % b)
